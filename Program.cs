d7400bd42b1586777f27d14c01ff45a98e1814a3
sworc <lightbox@nifty.com>
2021-10-28 16:05:09 +0900

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Web;
+using System.Text;
+using System.Collections.Generic;
+using System.Net;
+using System;
+
+namespace cs_1028
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost/php-form-post/keijiban6.php";
+            string encoding = "utf-8";
+            Dictionary<string, string> param
+             = new Dictionary<string, string>() {
+                 { "personal_name", "件名CS" },
+                 { "contents", "本文\nあいうえお" } 
+               };
+
+            try {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.GetEncoding("utf-8");
+                webClient.Headers["Content-Type"] = "application/x-www-form-urlencoded";
+
+                string data_string = "";
+                foreach (KeyValuePair<string, string> kvp in param) {
+                    if (data_string != "") {
+                        data_string += "&";
+                    }
+                    data_string += kvp.Key + "=";
+                    data_string += HttpUtility.UrlEncode(kvp.Value, Encoding.GetEncoding(encoding));
+                }
+
+                string result = webClient.UploadString(new Uri(url), "POST", data_string);
+
+                Console.WriteLine(result);
+
+            }
+            catch (System.Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
