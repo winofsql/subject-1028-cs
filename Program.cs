ca050eccc2e052f745631e42893740ef1a20e175
username <username@example.com>
2021-10-31 11:53:07 +0900

    ローカルで変更

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿using System.Web;
-using System.Text;
+﻿using System;
 using System.Collections.Generic;
 using System.Net;
-using System;
+using System.Text;
+using System.Web;
 
 namespace subject_1028_cs_post
 {
@@ -10,34 +10,43 @@ namespace subject_1028_cs_post
     {
         static void Main(string[] args)
         {
-            string url = "http://localhost/php-form-post/keijiban6.php";
+            string url = "http://localhost/php/keijiban.php";
             string encoding = "utf-8";
-            Dictionary<string, string> param
-             = new Dictionary<string, string>() {
-                 { "personal_name", "件名CS" },
-                 { "contents", "本文\nあいうえお" } 
-               };
+            Dictionary<string, string> param =
+                new Dictionary<string, string>()
+                {
+                    { "personal_name", "件名CS" },
+                    { "contents", "本文\nあいうえお" }
+                };
 
-            try {
+            try
+            {
                 WebClient webClient = new WebClient();
                 webClient.Encoding = Encoding.GetEncoding("utf-8");
-                webClient.Headers["Content-Type"] = "application/x-www-form-urlencoded";
+                webClient.Headers["Content-Type"] =
+                    "application/x-www-form-urlencoded";
 
                 string data_string = "";
-                foreach (KeyValuePair<string, string> kvp in param) {
-                    if (data_string != "") {
+                foreach (KeyValuePair<string, string> kvp in param)
+                {
+                    if (data_string != "")
+                    {
                         data_string += "&";
                     }
                     data_string += kvp.Key + "=";
-                    data_string += HttpUtility.UrlEncode(kvp.Value, Encoding.GetEncoding(encoding));
+                    data_string +=
+                        HttpUtility
+                            .UrlEncode(kvp.Value,
+                            Encoding.GetEncoding(encoding));
                 }
 
-                string result = webClient.UploadString(new Uri(url), "POST", data_string);
+                string result =
+                    webClient.UploadString(new Uri(url), "POST", data_string);
 
-                Console.WriteLine(result);
-
+                Console.WriteLine (result);
             }
-            catch (System.Exception ex) {
+            catch (System.Exception ex)
+            {
                 Console.WriteLine(ex.Message);
             }
         }
--- php/keijiban.php
+++ php/keijiban.php
@@ -1,82 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.1.3/css/bootstrap.min.css">
-    <title>PHP TEST</title>
-</head>
-<body>
-<h3 class="alert alert-primary">掲示板</h3>
-<div id="base" class="m-4">
-    <form method="POST" action="<?php print($_SERVER['PHP_SELF']) ?>">
-        <input type="text" name="personal_name"><br><br>
-        <textarea name="contents" rows="8" cols="40">
-        </textarea><br><br>
-        <input type="submit" name="btn1" value="投稿する">
-    </form>
-</div>
 <?php
+header( "Content-Type: text/html; charset=utf-8" );
+require_once("model.php");
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     writeData();
 }
 
-readData();
-
-function readData(){
-    $keijban_file = 'keijiban.txt';
-
-    $fp = fopen($keijban_file, 'rb');
-
-    if ($fp){
-        if (flock($fp, LOCK_SH)){
-            while (!feof($fp)) {
-                $buffer = fgets($fp);
-                print($buffer);
-            }
-
-            flock($fp, LOCK_UN);
-        }
-        else {
-            print('ファイルロックに失敗しました');
-        }
-    }
-
-    fclose($fp);
-}
-
-function writeData(){
-    $personal_name = $_POST['personal_name'];
-    $contents = $_POST['contents'];
-    $contents = nl2br($contents);
-    $now = date("Y/m/d H:i:s");
-
-    $data = <<< DATA
-<hr>
-<p>投稿者:{$personal_name} : {$now}</p>
-<p>内容:</p>
-<p>{$contents}</p>
-DATA;
-
-    $keijban_file = 'keijiban.txt';
-
-    $fp = fopen($keijban_file, 'ab');
-
-    if ($fp){
-        if (flock($fp, LOCK_EX)){
-            if (fwrite($fp,  $data) === FALSE){
-                print('ファイル書き込みに失敗しました');
-            }
-
-            flock($fp, LOCK_UN);
-        }
-        else {
-            print('ファイルロックに失敗しました');
-        }
-    }
-
-    fclose($fp);
-}
-
+require_once("view.php");
 ?>
-</body>
-</html>
